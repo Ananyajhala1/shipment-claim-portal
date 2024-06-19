c9d8a7d80e4f9b0e4e88be734b5005f741847ea7
CipherKT <ktcipher@gmail.com>
2024-06-19 12:49:38 +0530

    Sync database schema changes with models and context files

---
+++ Backend/ClaimsAPI/Models/Permission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClaimsAPI.Models;
+
+public partial class Permission
+{
+    public int PermissionId { get; set; }
+
+    public string? PermissionDescription { get; set; }
+
+    public bool? AllowClient { get; set; }
+
+    public bool? AllowCarrier { get; set; }
+
+    public bool? AllowInsurance { get; set; }
+
+    public virtual ICollection<RolePermission> RolePermissions { get; set; } = new List<RolePermission>();
+}
