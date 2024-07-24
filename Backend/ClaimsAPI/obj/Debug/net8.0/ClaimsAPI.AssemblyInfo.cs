633834d9ac2b47bf752e1b0de5c229faa71f4884
CipherKT <ktcipher@gmail.com>
2024-07-24 16:57:10 +0530

    generated debug files

--- Backend/ClaimsAPI/obj/Debug/net8.0/ClaimsAPI.AssemblyInfo.cs
+++ Backend/ClaimsAPI/obj/Debug/net8.0/ClaimsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClaimsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186943d673e04714d234e4df1f813fc6a35cac7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e4f9643b13c3c8db4a23540dac74643f4b86b3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClaimsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClaimsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
