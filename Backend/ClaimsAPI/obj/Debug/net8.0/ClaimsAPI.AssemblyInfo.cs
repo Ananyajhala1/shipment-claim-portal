1742e05e752c181cd5ca50001c614b1f5171b78b
Nyashavasoya <nyashavasoya@gmail.com>
2024-07-31 18:35:19 +0530

    changed config file

--- Backend/ClaimsAPI/obj/Debug/net8.0/ClaimsAPI.AssemblyInfo.cs
+++ Backend/ClaimsAPI/obj/Debug/net8.0/ClaimsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClaimsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87fb621ce5bb32e545d8ed752a1771aeeb5816d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875ae59f380a430642698754d35edc6b698f42f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClaimsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClaimsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
