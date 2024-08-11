10591849af743785b91c223c3a63ba779ea3217d
Ananyajhala1 <142742068+Ananyajhala1@users.noreply.github.com>
2024-08-11 16:23:01 +0530

    added  business rules

--- Backend/ClaimsAPI/obj/Debug/net8.0/ClaimsAPI.AssemblyInfo.cs
+++ Backend/ClaimsAPI/obj/Debug/net8.0/ClaimsAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClaimsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ab5125c2e1f9318706caf068ce89f5fadbd6e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503bde623e40d96d2d757626ca0fc15cea0a6a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClaimsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClaimsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
