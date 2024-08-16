35f2c2b2edcd79e305d457f4befe9126183d5585
CipherKT <ktcipher@gmail.com>
2024-08-16 12:13:06 +0530

    added getInsurance with clientValidation

--- Backend/ClaimsAPI/Controllers/CompanyController.cs
+++ Backend/ClaimsAPI/Controllers/CompanyController.cs
@@ -5,34 +5,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ClaimsAPI.Models.DTO.CompanyDTO;
 using ClaimsAPI.Service.company;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Logging;
 
 namespace ClaimsAPI.Controllers
 {
-    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CompanyController : ControllerBase
     {
         private readonly ICompany _company;
-        private readonly RequestTokenInfo _tokenInfo;
-        private readonly ILogger<CompanyController> _logger;
 
-        public CompanyController(ICompany company, RequestTokenInfo tokenInfo, ILogger<CompanyController> logger)
+        public CompanyController(ICompany company)
         {
             this._company = company;
-            _tokenInfo = tokenInfo;
-            _logger = logger;
         }
 
-
         [HttpGet]
-        
         public async Task<IActionResult> GetCompanies()
         {
-            _logger.LogInformation($"GetCompanies called by UserId: {_tokenInfo.userId}, ClientId: {_tokenInfo.clientId}");
-            var companies = await _company.GetCompanies(int.Parse(_tokenInfo.clientId));
+            var companies = await _company.GetCompanies();
             if (companies == null)
             {
                 return NotFound();
@@ -40,8 +30,32 @@ namespace ClaimsAPI.Controllers
             return Ok(companies);
         }
 
+
+
+        [HttpGet("getCarrier")]
+        public async Task<IActionResult> GetCarrier(int clientid)
+        {
+            var carriers = await _company.GetCarrier(clientid);
+            if (carriers == null)
+            {
+                return NotFound();
+            }
+            return Ok(carriers);
+        }
+        [HttpGet("getInsurance")]
+        public async Task<IActionResult> GetInsurance(int clientid)
+        {
+            var carriers = await _company.GetInsurance(clientid);
+            if (carriers == null)
+            {
+                return NotFound();
+            }
+            return Ok(carriers);
+        }
         [HttpGet]
+
         [Route("{id:int}")]
+
         public async Task<IActionResult> GetCompanyById(int id)
         {
             var company = await _company.GetCompanyById(id);
@@ -56,7 +70,7 @@ namespace ClaimsAPI.Controllers
 
         public async Task<IActionResult> AddCompany(CompanyPostDTO company)
         {
-            var Company = await _company.AddCompany(company, int.Parse(_tokenInfo.clientId));
+            var Company = await _company.AddCompany(company);
             return Ok(company);
         }
 
@@ -65,13 +79,13 @@ namespace ClaimsAPI.Controllers
 
         public async Task<IActionResult> UpdateCompany(int id, CompanyUpdateDTO company)
         {
-           
+
             var Company = await _company.UpdateCompany(id, company);
             if (Company == null)
             {
                 return BadRequest("Company does not exist");
             }
-            
+
             return Ok(Company);
         }
 
@@ -85,7 +99,7 @@ namespace ClaimsAPI.Controllers
             {
                 return BadRequest();
             }
-            
+
             return Ok(company);
         }
     }
---
+++ Backend/ClaimsAPI/businessRulesValidator/ClientValidatorBRule.cs
@@ -0,0 +1,37 @@
+﻿using ClaimsAPI.Models;
+
+namespace ClaimsAPI.businessRulesValidator
+{
+    public class ClientValidatorBRule : BRuleBase
+    {
+
+        private readonly RequestTokenInfo _requestTokenInfo;
+        private readonly ShipmentClaimsContext _shipmentClaimsContext;
+        private int _clientid;
+        public ClientValidatorBRule(ShipmentClaimsContext shipmentClaimsContext, int clientid, RequestTokenInfo requestTokenInfo) : base(shipmentClaimsContext)
+        {
+
+            _shipmentClaimsContext = shipmentClaimsContext;
+            _clientid = clientid;
+            _requestTokenInfo = requestTokenInfo;
+
+
+
+        }
+        public override bool Execute()
+        {
+
+            if (_clientid.ToString() != _requestTokenInfo.clientId)
+            {
+                this.error = "aupplied client id doesnt match with login user";
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+
+        }
+    }
+}
