5b20f2b628d0e4d2674136e8af01353bd5f26f1e
Nyashavasoya <nyashavasoya@gmail.com>
2024-08-16 22:54:40 +0530

    business rules validator added

---
+++ Backend/ClaimsAPI/businessRulesValidator/BRuleBase.cs
@@ -0,0 +1,35 @@
+﻿using ClaimsAPI.Models;
+using ClaimsAPI.Models.Entites;
+using ClaimsAPI.Models.DTO.CompanyTypeDTO;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+
+
+
+
+
+
+
+namespace ClaimsAPI.businessRulesValidator
+{
+    public abstract class BRuleBase
+    {
+
+        public string error { get; set; }
+
+        private readonly ShipmentClaimsContext _shipmentClaimsContext;
+
+
+        public BRuleBase(ShipmentClaimsContext shipmentClaimsContext)
+        {
+            _shipmentClaimsContext = shipmentClaimsContext;
+
+        }
+
+        public abstract bool Execute();
+
+
+    }
+
+
+    }
---
+++ Backend/ClaimsAPI/businessRulesValidator/BRuleRunner.cs
@@ -0,0 +1,66 @@
+﻿namespace ClaimsAPI.businessRulesValidator
+{
+    public class BRuleRunner
+    {
+
+        IList<string> Errors = new List<string>();
+
+
+
+        private IList<BRuleBase> _Brules;
+
+
+        public BRuleRunner(IList<BRuleBase> bRules)
+        {
+            _Brules = bRules;
+        }
+
+
+
+       public void ExecuteRules()
+        {
+            foreach ( var Brule in _Brules)
+            {
+                if (!Brule.Execute())
+                {
+                    Errors.Add(Brule.error);
+                }
+
+            }
+        }
+
+
+
+        public bool hasError()
+        {
+            if(Errors != null && Errors.Count>0 )
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public IList<string> getErrorList()
+        {
+
+            return Errors;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
